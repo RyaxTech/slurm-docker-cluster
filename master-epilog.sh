bed79d30076168b5609d111576d50d7d37175a72
Michael Mercier <michael.mercier@ryax.tech>
2022-12-16 09:03:05 +0100

    first bebida implentation

---
+++ master-epilog.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+export SCRIPT_PATH=$(cd $(dirname $0) >/dev/null 2>&1 && pwd )
+(
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export KUBECONFIG=$SCRIPT_PATH/../etc/bebida/workflow-manager-1-kubeconfig.yaml
+
+printenv
+export SLURM_NODELIST=$SLURM_JOB_NODELIST
+for node in $(scontrol show hostnames)
+do
+        kubectl uncordon $node
+done
+) > $SCRIPT_PATH/../logs/${SLURM_JOB_ID}-epilog-logs.out 2> $SCRIPT_PATH/../logs/${SLURM_JOB_ID}-epilog-logs.err
---
+++ master-prolog.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+export SCRIPT_PATH=$(cd $(dirname $0) >/dev/null 2>&1 && pwd )
+(
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export KUBECONFIG=$SCRIPT_PATH/../etc/bebida/workflow-manager-1-kubeconfig.yaml
+
+printenv
+export SLURM_NODELIST=$SLURM_JOB_NODELIST
+for node in $(scontrol show hostnames)
+do
+        kubectl drain --force --grace-period=5 --ignore-daemonsets --delete-emptydir-data $node
+done
+) > $SCRIPT_PATH/../logs/${SLURM_JOB_ID}-prolog-logs.out 2> $SCRIPT_PATH/../logs/${SLURM_JOB_ID}-prolog-logs.err
